f570b860631f3fa197beb49da04ba2726e5f90df
lucasalonso64 <alonsosistemas@gmail.com>
2020-04-08 11:53:21 -0300

    0.2

---
+++ frotend/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState } from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+import { func } from 'prop-types';
+
+function App() {
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
